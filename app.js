83990ad21da4ec37a6fc741baaed5ca5b1a98007
vishal <vishaliscoding27@gmail.com>
2023-04-19 21:33:42 +0530

    second commit

--- app.js
+++ app.js
@@ -15,3 +15,38 @@ function openORclose(id) {
   }
   nav_con.classList.toggle("responsiveheight");
 }
+var quantity = document.querySelector("#quantity");
+var toprint = document.querySelector(".toprint");
+var img_main = document.querySelector(".main-img");
+const tochangeimg = document.querySelector(".product_image_container");
+const dialog = document.querySelector("dialog");
+function changevalue() {
+  toprint.innerHTML = `cost : ${60 * quantity.value}₹`;
+}
+function cangeimg(e) {
+  img_main.src = e;
+}
+function closedialog(id) {
+  console.log(id);
+  if (id == "open") {
+    dialog.show();
+    disableScroll();
+  } else {
+    dialog.close();
+    enableScroll();
+  }
+}
+function disableScroll() {
+  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  (scrollLeft = window.pageXOffset || document.documentElement.scrollLeft),
+    (window.onscroll = function () {
+      window.scrollTo(scrollLeft, scrollTop);
+    });
+}
+
+function enableScroll() {
+  window.onscroll = function () {};
+}
+function defaultclose() {
+  dialog.close();
+}
