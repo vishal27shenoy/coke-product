16899c60893a66035239bea86cb97d0593f2972f
vishal <vishaliscoding27@gmail.com>
2023-04-20 12:23:28 +0530

    completed

--- app.js
+++ app.js
@@ -37,11 +37,11 @@ function closedialog(id) {
   }
 }
 function disableScroll() {
-  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  (scrollLeft = window.pageXOffset || document.documentElement.scrollLeft),
-    (window.onscroll = function () {
-      window.scrollTo(scrollLeft, scrollTop);
-    });
+  // scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  // (scrollLeft = window.pageXOffset || document.documentElement.scrollLeft),
+  //   (window.onscroll = function () {
+  //     window.scrollTo(scrollLeft, scrollTop);
+  //   });
 }
 
 function enableScroll() {
